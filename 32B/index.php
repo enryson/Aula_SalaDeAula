8c6b4f4cfafa67eb37009fd3f11adb5e47405702
enryson <enryson@gmail.com>
2019-05-07 01:26:11 -0300

    Atualizando

--- 32B/index.php
+++ 32B/index.php
@@ -2,14 +2,16 @@
 
 include_once("config.php");
 
-$result = mysqli_query($mysqli, "SELECT * FROM pessoas ORDER BY cpf DESC"); 
+$result = mysqli_query($mysqli, "SELECT * FROM pessoas ORDER BY cpf ASC"); 
 
 ?>
 
 <html>
 <body>
+
+      
 	<?php
-		echo "<a href=\"edit.php?new=1\">adicionar</a>";
+		echo "<a href=\"edit.php\">adicionar</a>";
 	?>
 	<table width='80%' border=0>
 	<tr bgcolor='#CCCCCC'>
@@ -27,11 +29,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM pessoas ORDER BY cpf DESC");
         echo "<td>".$res['p_sobrenome']."</td>";
         echo "<td>".$res['p_nascimento']."</td>";
 		echo "<td>
-		<a href=\"edit.php?cpf=$res[cpf]\">Edit</a> | 
-
-		
-		<a href=\"delete.php?id=$res[cpf]\" 
-		onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
+		<a href=\"edit.php?cpf=$res[cpf]\">Edit</a></td>";		
 	}
 	?>
 	</table>
