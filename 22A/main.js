f205a409afe7a19669bdd318890f68587f8d1a01
Enryson Oliveira Ferraz <enryson@gmail.com>
2019-05-07 18:33:21 -0300

    Update 32A

---
+++ 22A/main.js
@@ -0,0 +1,33 @@
+//variavel do quiz
+var quiz = document.getElementById("quiz");
+//variavel do enunciado
+var ques = document.getElementById("question");
+// variaveis das alternativas
+var opt1 = document.getElementById("option1");
+var opt2 = document.getElementById("option2");
+var opt3 = document.getElementById("option3");
+var opt4 = document.getElementById("option4");
+// variavel resultado
+var res = document.getElementById("result");
+//botoes pra sair e proxima pergunta
+var nextbutton = document.getElementById("next");
+var q = document.getElementById('quit');
+// varavel com a quantidade de perguntas
+var tques = questions.length;
+// varaivel com o valor de acertos
+var score=0;
+// variavel contadora com a pegunta atual
+var quesindex=0;
+
+function quit(){
+    quiz.style.display='none';
+    result.style.display='';
+    q.style.display="none";
+    var f = score/tques;
+    result.textContent="SCORE ="+f*100;    
+}
+
+function give_ques(quesindex){
+    ques.textContent=quesindex+1+". "+questions[quesindex][0];
+
+}
--- 32A/edit.php
+++ 32A/edit.php
@@ -1,61 +1,115 @@
 <?php
 include_once("config.php");
 
+
 $id = $_GET['id'];
+if (empty($id)) {
+	$id = '';
+	$name = '';
+	$age = '';
+	$email = '';
+}else{
+	$result = mysqli_query($mysqli, "SELECT * FROM users WHERE id=$id");
 
-$result = mysqli_query($mysqli, "SELECT * FROM users WHERE id=$id");
-
-while($res = mysqli_fetch_array($result))
-{
-    $id = $res['id'];
-    $name = $res['name'];
-    $age = $res['age'];
-    $email = $res['email'];
+	while ($res = mysqli_fetch_array($result)) {
+		$id = $res['id'];
+		$name = $res['name'];
+		$age = $res['age'];
+		$email = $res['email'];
+	}
 }
 
-if(isset($_POST['update']))
-{
-	$id = mysqli_real_escape_string($mysqli, $_POST['id']);	
+
+if (isset($_POST['add'])) {
+	$id = mysqli_real_escape_string($mysqli, $_POST['id']);
 	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
 	$age = mysqli_real_escape_string($mysqli, $_POST['age']);
-	$email = mysqli_real_escape_string($mysqli, $_POST['email']);	
-	
-	if(empty($id) || empty($name) || empty($age) || empty($email)) {		
-		if(empty($id)) {
-			echo "<font color='red'>nao deixe em branco</font><br/>";
-		}	
-		if(empty($name)) {
+	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+
+	if (empty($id) || empty($name) || empty($age) || empty($email)) {
+		if (empty($id)) {
 			echo "<font color='red'>nao deixe em branco</font><br/>";
 		}
-		
-		if(empty($age)) {
+		if (empty($name)) {
 			echo "<font color='red'>nao deixe em branco</font><br/>";
 		}
-		
-		if(empty($email)) {
+
+		if (empty($age)) {
 			echo "<font color='red'>nao deixe em branco</font><br/>";
-		}		
-	} else {	
+		}
+
+		if (empty($email)) {
+			echo "<font color='red'>nao deixe em branco</font><br/>";
+		}
+	} else {
 		//updating the table
-		$result = mysqli_query($mysqli, "UPDATE pessoas SET name='$name',age='$age',email='$email' WHERE id=$id");
-		
+		$id=null;
+		$result = mysqli_query($mysqli, "INSERT INTO pessoas VALUES('$id','$name','$age','$email')");
 		//redirectig to the display page. In our case, it is index.php
 		header("Location: index.php");
 	}
 }
+
+if (isset($_POST['update'])) {
+	$id = mysqli_real_escape_string($mysqli, $_POST['id']);
+	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
+	$age = mysqli_real_escape_string($mysqli, $_POST['age']);
+	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+
+	if (empty($id) || empty($name) || empty($age) || empty($email)) {
+		if (empty($id)) {
+			echo "<font color='red'>nao deixe em branco</font><br/>";
+		}
+		if (empty($name)) {
+			echo "<font color='red'>nao deixe em branco</font><br/>";
+		}
+
+		if (empty($age)) {
+			echo "<font color='red'>nao deixe em branco</font><br/>";
+		}
+
+		if (empty($email)) {
+			echo "<font color='red'>nao deixe em branco</font><br/>";
+		}
+	} else {
+		//updating the table
+		$result = mysqli_query($mysqli, "UPDATE users SET name='$name',age='$age',email='$email' WHERE id=$id");
+
+		//redirectig to the display page. In our case, it is index.php
+		header("Location: index.php");
+	}
+}
+
+if (isset($_POST['delete'])) {
+	$id = $_POST['id'];
+	$result = mysqli_query($mysqli, "DELETE FROM users WHERE id=$id");
+
+	header("Location: index.php");
+}
 ?>
 
 <html>
-<body>
-    <h3>id</h3>
-    <input type="text" name="id" value="<?php echo $id?>">
-    <h3>name</h3>
-    <input type="text" name="name" value="<?php echo $name?>">
-    <h3>age</h3>
-    <input type="text" name="age" value="<?php echo $age?>">
-    <h3>email</h3>
-    <input type="text" name="email" value="<?php echo $email?>">
 
-    <input type="submit" name="update" value="Update">
+<body>
+	<form name="form1" method="post" action="edit.php">
+		<h3>id</h3>
+		<input type="text" name="id" value="<?php echo $id ?>">
+		<h3>name</h3>
+		<input type="text" name="name" value="<?php echo $name ?>">
+		<h3>age</h3>
+		<input type="text" name="age" value="<?php echo $age ?>">
+		<h3>email</h3>
+		<input type="text" name="email" value="<?php echo $email ?>">
+
+		<?php
+		if (empty($id)) {
+			echo '<input type="submit" name="add" value="ADD">';
+		} else {
+			echo '<input type="submit" name="update" value="Update">';
+			echo '<input type="submit" name="delete" value="Delete">';
+		}
+		?>
+	</form>
 </body>
+
 </html>
